5f7796ddedcbb9d4fdb5f6ba35d2f13d8cc0c780
Felipe Hansen <felipehansen4@gmail.com>
2022-09-25 20:57:33 -0300

    Aula4

--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule Wabanex.MixProject do
       {:gettext, ">= 0.0.0"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:crudry, "~> 2.4.0"},
       {:absinthe, "~> 1.5.0"},
       {:absinthe_plug, "~> 1.5"}
     ]
